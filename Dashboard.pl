fcdfc8970572ab31a383cc95e7d71897040a865c
sailingfree <peter@naiadhome.com>
2022-08-29 21:57:03 +0100

    Touch screen

--- Dashboard.pl
+++ Dashboard.pl
@@ -14,6 +14,12 @@ use Date::Parse;
 
 use RRDTool::OO;
 
+use threads;
+use threads::shared;
+
+use Time::HiRes qw(usleep);
+
+
 our $RUNNING = 1;
 sub sighan {
 	$RUNNING = 0; 
@@ -21,23 +27,88 @@ sub sighan {
 	exit;
 }
 our $fb = Graphics::Framebuffer->new('SPLASH'=>0,'SHOW_ERRORS'=>1, 'ACCELERATED'=>1, 'RESET'=>0);
+$fb->graphics_mode();
 #$fb->cls('OFF'); # Clear screen and turn off the console cursor
 $SIG{'QUIT'} = \&sighan;
 $SIG{'INT'} = \&sighan;
 $SIG{'HUP'} = \&sighan;
 $SIG{'TERM'} = \&sighan;
 
-my $xres = $fb->{'XRES'};
-my $yres = $fb->{'YRES'};
+my $xres :shared = $fb->{'XRES'};
+my $yres :shared = $fb->{'YRES'};
 my @background = {'red' => 160, 'green' => 160, 'blue' => 160, 'alpha' => 255};
 my @black = {'red' => 0, 'green' => 0, 'blue' => 0, 'alpha' => 255};
 my @box_color = {'red' => 255, 'green' => 0, 'blue' => 0, 'alpha' => 255};
 my @gauge_color = {'red' => 200, 'green' => 200, 'blue' => 200, 'alpha' => 255};
+my @mouse_color = {'red' => 64, 'green' => 64, 'blue' => 64, 'alpha' => 64};
+
+my %mouse_data :shared;
+my $mouse_lock :shared;
+
+# Handler for the AR110 mouse handler in raw mode.
+# This is very specific to this device on the RPi
+# Handler for an AR110 in raw HID mode (tocuh mode generic in data sheet)
+# This gives 5 byte packets and the x and y are 13 bits - 8192 max
+# 0->button state  0x80 == pen up 0x81 = pen down
+# 1->x 8 bits
+# 2->x top 5 bits
+# 3->y 8 bits
+# 4->y top 5 bits
+# the result is scaled to the screen dimensions (hard coded)
+# This is a thread and runs async to the main code.
+# It uses a shared data structure with a lock 
+sub mouse_handler {
+	print STDOUT "Starting the mouse handler ...\n";
+	my $fbth = Graphics::Framebuffer->new('SPLASH'=>0,'SHOW_ERRORS'=>1, 'ACCELERATED'=>1, 'RESET'=>0);
+	do {
+		open(my $m, '<', '/dev/hidraw2');
+		binmode($m);
+		my $mouse = '';
+		my $len = sysread($m, $mouse, 5);
+		if($len ne 5) {
+			print STDOUT "Only got $len bytes, expected 5\n";
+		} else {
+			my($b, $xl,$xh,$yl,$yh) = unpack('c5', $mouse);
+			if(! ($b & 0x01)) {
+				# Only process button up events to avoid double counting
+				my $x = $xl + (256 * $xh);
+				my $y = $yl + (256 * $yh);
+				$x = $x * 800 / 8192;
+				$y = $y * 480 / 8192;
+				printf STDOUT "B 0x%X  X %d Y %d\n", $b, $x, $y;
+				lock($mouse_lock); 
+				{
+					if(!$mouse_data{'move'}) {
+						$mouse_data{'move'} = 1;
+						$mouse_data{'x'} = $x;
+						$mouse_data{'y'} = $y;
+						
+						# pop up a circle to show touch event
+						my $mouse_radius = 32;
+						
+						# Copy the area under the touch point
+						my $mouse_area = $fbth->blit_read({'x'=>$x - $mouse_radius, 
+								'y'=>$y - $mouse_radius,
+								'width'=>$mouse_radius * 2, 
+								'height'=>$mouse_radius * 2});
+						$fbth->set_color(@mouse_color);
+						$fbth->circle({'x'=>$x, 'y'=>$y, 'radius'=>32,'filled'=>1});
+						usleep(1000 * 200);
+						# Restore the colour and the area just painted
+						$fbth->set_color(@background_color);
+						$fbth->blit_write($mouse_area);
+					}
+				}
+			} else {
+				printf STDOUT "B 0x%X  X %d Y %d\n", $b, $x, $y;
+			}
+		}
+		close $m;
+	} while(true);
+}
 
 
 # Create the main background and fill it.§
-#
-#
 sub drawBackground {
 	$fb->normal_mode();
 	$fb->set_color(@background);
@@ -52,92 +123,139 @@ my $face = 'LiberationSans-Bold.ttf';
 $path = '/usr/share/fonts/truetype/quicksand';
 $face = 'Quicksand-Medium.ttf';
 
-my $sog,$wspeed, $wangle, $d, $hdg, $dateTimeDisp, $temp, $press, $windy, $houseBatt, $engineBat, $net;
+#my $sog,$wspeed, $wangle, $d, $hdg, $dateTimeDisp, $temp, $press, $windy, $houseBatt, $engineBat, $net;
 
+my %instruments;
 
 sub createInstruments {
-	$sog = Numeric->new('SOG', 0, 0, $xres/3 ,$yres/3,'SOG (kts)');
-	$wspeed = Numeric->new('TWS', $xres/3, 0, $xres/3 ,$yres/3,'Wind (A kts)');
-	$wangle = Numeric->new('TWA', 2* $xres/3, 0, $xres/3 ,$yres/3,'Wind Angle (A)');
-	$d = Numeric->new('DPT', 0, $yres/3, $xres/3 ,$yres/3,'Depth (m)');
-	$hdg = Numeric->new('HDG', $xres/3, $yres/3, $xres/3 ,$yres/3,'Heading');
-	$dateTimeDisp = Numeric->new('DATE', 0, 2*$yres/3, $xres/3, $yres/6, 'Date/Time'); 
-	$temp = Numeric->new('TMP', 0, 5*$yres/6, $xres/6, $yres/6,'Temp (C)');
-	$press = Numeric->new('PRESS', $xres/6, 5*$yres/6, $xres/6, $yres/6,'Press (Pa)');
+	$instruments{'SOG'} = Numeric->new('SOG', 0, 0, $xres/3 ,$yres/3,'SOG (kts)');
+	$instruments{'TWS'} = Numeric->new('TWS', $xres/3, 0, $xres/3 ,$yres/3,'Wind (A kts)');
+	$instruments{'TWA'} = Numeric->new('TWA', 2* $xres/3, 0, $xres/3 ,$yres/3,'Wind Angle (A)');
+	$instruments{'DPT'} = Numeric->new('DPT', 0, $yres/3, $xres/3 ,$yres/3,'Depth (m)');
+	$instruments{'HDG'} = Numeric->new('HDG', $xres/3, $yres/3, $xres/3 ,$yres/3,'Heading');
+	$instruments{'DATE'} = Numeric->new('DATE', 0, 2*$yres/3, $xres/3, $yres/6, 'Date/Time'); 
+	$instruments{'TEMP'} = Numeric->new('TEMP', 0, 5*$yres/6, $xres/6, $yres/6,'Temp (C)');
+	$instruments{'PRESS'} = Numeric->new('PRESS', $xres/6, 5*$yres/6, $xres/6, $yres/6,'Press (Pa)');
 
-	$windy = Dial->new(2*$xres/3, $yres/3, $xres/3, 2*$yres/3, "");
-	$houseBatt = Numeric->new('BAT1', 2*$xres/6, 2*$yres/3, $xres/6, $yres/6, "House V");
-	$engineBatt = Numeric->new('BAT2', 3*$xres/6, 2*$yres/3, $xres/6, $yres/6, "Engine V");
-	$net = Numeric->new('NET', 2*$xres/6, 5*$yres/6, $xres/3, $yres/6, 'Network');
+	$instruments{'WIND'} = Dial->new(2*$xres/3, $yres/3, $xres/3, 2*$yres/3, "");
+	$instruments{'BAT1'} = Numeric->new('BAT1', 2*$xres/6, 2*$yres/3, $xres/6, $yres/6, "House V");
+	$instruments{'BAT2'} = Numeric->new('BAT2', 3*$xres/6, 2*$yres/3, $xres/6, $yres/6, "Engine V");
+	$instruments{'NET'} = Numeric->new('NET', 2*$xres/6, 5*$yres/6, $xres/3, $yres/6, 'Network');
 }
 # Create the instruments to display
 drawBackground();
 createInstruments();
 
-# Main look to read the signal k source parse the result and display
-#
+# Create the mouse thread
+my $mouse_thread = threads->create(\&mouse_handler);
+$mouse_thread->detach();
 
-# Main loop to read the signal k source parse the result and display
+
+# Signalk handler to read the signal k source parse the result and display
 # the instruments
 sub signalk_handler {
-	do {
-		my $ua = LWP::UserAgent->new;
-		my $url = 'http://localhost:3000/signalk/v1/api/';
-		my $req = HTTP::Request->new(GET=>$url);
-		$req->header('content-type'=>'application/json');
-		my $resp = $ua->request($req);
-		my $msg = $resp->decoded_content;
-		my $sc = decode_json($msg);
-		$Data::Dumper::Indent = 1;
-		my $urn = $sc->{'self'};
-		$urn =~ s/vessels\.//;
-#		print STDOUT Dumper($sc);
+	my $ua = LWP::UserAgent->new;
+	my $url = 'http://localhost:3000/signalk/v1/api/';
+	my $req = HTTP::Request->new(GET=>$url);
+	$req->header('content-type'=>'application/json');
+	my $resp = $ua->request($req);
+	my $msg = $resp->decoded_content;
+	my $sc = decode_json($msg);
+	$Data::Dumper::Indent = 1;
+	my $urn = $sc->{'self'};
+	$urn =~ s/vessels\.//;
+	#		print STDOUT Dumper($sc);
 
-		my $temperatureOutside = $sc->{'vessels'}->{$urn}->{'environment'}->{'outside'}->{'temperature'}->{'value'};
-		my $pressureOutside = $sc->{'vessels'}->{$urn}->{'environment'}->{'outside'}->{'pressure'}->{'value'};
-		my $depth = $sc->{'vessels'}->{$urn}->{'environment'}->{'depth'}->{'belowTransducer'}->{'value'};
+	my $temperatureOutside = $sc->{'vessels'}->{$urn}->{'environment'}->{'outside'}->{'temperature'}->{'value'};
+	my $pressureOutside = $sc->{'vessels'}->{$urn}->{'environment'}->{'outside'}->{'pressure'}->{'value'};
+	my $depth = $sc->{'vessels'}->{$urn}->{'environment'}->{'depth'}->{'belowTransducer'}->{'value'};
 
-		my $wind = $sc->{'vessels'}->{$urn}->{'environment'}->{'wind'}->{'speedApparent'}->{'value'};
-		my $windangle = $sc->{'vessels'}->{$urn}->{'environment'}->{'wind'}->{'angleApparent'}->{'value'};
-		my $speedoverground = $sc->{'vessels'}->{$urn}->{'navigation'}->{'speedOverGround'}->{'value'};
-		my $headingTrue = $sc->{'vessels'}->{$urn}->{'navigation'}->{'headingTrue'}->{'value'};
-		my $dateTime = $sc->{'vessels'}->{$urn}->{'navigation'}->{'headingTrue'}->{'timestamp'};
+	my $wind = $sc->{'vessels'}->{$urn}->{'environment'}->{'wind'}->{'speedApparent'}->{'value'};
+	my $windangle = $sc->{'vessels'}->{$urn}->{'environment'}->{'wind'}->{'angleApparent'}->{'value'};
+	my $speedoverground = $sc->{'vessels'}->{$urn}->{'navigation'}->{'speedOverGround'}->{'value'};
+	my $headingTrue = $sc->{'vessels'}->{$urn}->{'navigation'}->{'headingTrue'}->{'value'};
+	my $dateTime = $sc->{'vessels'}->{$urn}->{'navigation'}->{'headingTrue'}->{'timestamp'};
 
-		my $essid = `/usr/sbin/iwconfig 2>&1|grep wlan|sed "s/:/ /" | awk '{print \$5}'`;
-		chomp($essid);
-		$d->updateFloat($depth, 1);
-		$wspeed->updateFloat($wind, 1.97);	
-		$wangle->updateAngle($windangle);
-		$sog->updateFloat($speedoverground, 1.97);	## Convert m/s to knots
-		$hdg->updateAngle($headingTrue);
-		my($ss,$mm,$hh,$day,$month,$year,$zone) = strptime($dateTime);
-		$dateTimeDisp->updateText("$hh:$mm:" . int($ss));
-		$windy->update($windangle * 57.29);
-		$temp->update($temperatureOutside);
-		$press->updateFloat($pressureOutside, 0.01);
-		$houseBatt->updateFloat(12.12,1);
-		$engineBatt->updateFloat(13.92,1);
-		$net->update($essid);
-		sleep(1);
+	my $essid = `/usr/sbin/iwconfig 2>&1|grep wlan|sed "s/:/ /" | awk '{print \$5}'`;
+	chomp($essid);
+	$instruments{'DPT'}->updateFloat($depth, 1);
+	$instruments{'TWS'}->updateFloat($wind, 1.97);	
+	$instruments{'TWA'}->updateAngle($windangle);
+	$instruments{'SOG'}->updateFloat($speedoverground, 1.97);	# Convert m/s to knots
+	$instruments{'HDG'}->updateAngle($headingTrue);
+	my($ss,$mm,$hh,$day,$month,$year,$zone) = strptime($dateTime);
+	$instruments{'DATE'}->updateText("$hh:$mm:" . int($ss));
+	$instruments{'WIND'}->update($windangle * 57.29);
+	$instruments{'TEMP'}->updateFloat($temperatureOutside, 1);
+	$instruments{'PRESS'}->updateFloat($pressureOutside, 0.01);
+	$instruments{'BAT1'}->updateFloat(12.12,1);
+		$instruments{'BAT2'}->updateFloat(13.92,1);
+		$instruments{'NET'}->update($essid);
 
-		# Load the image
-		#my $img = $fb->load_image({'file'=>"rrd/DPT.png", 
-		#		'center'=>CENTER_XY});
 
-		# Copy the area to be drawn over
-		#$old = $fb->blit_read({'x'=>$img->{'x'}, 
-		#		'y'=>$img->{'y'},
-		#		'width'=>$img->{'width'}, 
-		#		'height'=>$img->{'height'}});
-##
-		#$fb->blit_write($img);
-		#sleep(1);
-		#$fb->blit_write($old);
-
-	} while(1);
 }
-signalk_handler();
 
+
+# Main loop to handle the mouse and update the data
+#
+# It looks for any mouse events and handles those.
+# The mouse events can pop up a graph or other pages
+# If a graph is shown then no signalk updates are done.
+# If a graph is showing then it is removed and an update is done
+#
+# old contains either 0 or an area to be restored
+# if 0 then noting to be restored and we can draw a graph if wanted
+# if not zero then restore the old area and continue as normal
+my $old = 0;
+
+my $last = time; # last signalk update time
+
+do {
+	{
+		# Process any mouse events;
+		lock($mouse_lock);
+		if($mouse_data{'move'}) {
+			my $x = $mouse_data{'x'};
+			my $y = $mouse_data{'y'};
+			if($old) {
+				# We have something showing so remove it
+				# Restore the original contents
+				$fb->blit_write($old);
+				$old = 0;
+			} else {
+				# Nothing showing so find the area affected
+				foreach my $key (keys %instruments) {
+					my $i = %instruments{$key};
+					if($i->inrect($x, $y)) {
+						print STDOUT "Found " . $i->{'name'} . "\n";
+						# Load the image
+						my $img = $fb->load_image({'file'=> $i->image_path(), 
+								'width' => $xres * 0.9,
+								'scale_type' => 'max',
+								'center'=>CENTER_XY});
+						if($img) {
+							# Copy the area to be drawn over
+							$old = $fb->blit_read({'x'=>$img->{'x'}, 
+									'y'=>$img->{'y'},
+									'width'=>$img->{'width'}, 
+									'height'=>$img->{'height'}});
+							#
+							$fb->blit_write($img);
+						}
+					}
+
+				}
+			}
+			$mouse_data{'move'} = 0;
+
+		}
+		if(!$old &&  time > $last + 1) {
+			signalk_handler($old);
+			$last = time;
+		}
+	}
+	usleep(100 * 1000);
+} while($RUNNING);
 
 print STDOUT "All done\n";
 print STDOUT "Done\n";
@@ -203,13 +321,14 @@ print STDOUT "Done\n";
 		my $rrd = RRDTool::OO->new(
 			file => 'rrd/' . $self->{'name'} . "_rrd.rrd" );
 
+
 		# Create a round-robin database
 		my $rows = 5;    # 
 		$rrd->create(
 			step        => 10,  # intervals
 			data_source => { name => $self->{'name'} . "_data",
-			type      => "GAUGE",
-	       		min =>0	},
+				type      => "GAUGE",
+				min =>0	},
 			archive     => { rows      => 60 });
 
 		$self->{'rrd'} = $rrd;
@@ -259,6 +378,11 @@ print STDOUT "Done\n";
 		}		
 	}
 
+	sub image_path {
+		my $self = shift;
+		return "rrd/" . $self->{'name'} . ".png";
+	}
+
 	sub updateRRD {
 		my $self = shift;
 		my $value = shift;
@@ -266,7 +390,7 @@ print STDOUT "Done\n";
 		my $rrd = $self->{'rrd'};
 		if(time - $self->{'lastrrd'} >= 10) {
 			$rrd->update($value);
-			$rrd->graph(image => "rrd/" . $self->{'name'} . ".png",
+			$rrd->graph(image => $self->image_path(), 
 				vertical_label => $self->{'name'},
 				start => time() - 600,
 				draw => { thickness => 2,
@@ -341,6 +465,21 @@ print STDOUT "Done\n";
 
 			$self->{'lastValue'} = $value;
 
+		}
+	}
+
+	sub inrect {
+		my $self = shift;
+		my $x = shift;
+		my $y = shift;
+		my $sx = $self->{'x'};
+		my $sy = $self->{'y'};
+		my $sw = $self->{'width'};
+		my $sh = $self->{'height'};
+		if($x >= $sx && $x <= $sx + $sw && $y >= $sy && $y <= $sy + $sh) {
+			return 1;
+		} else {
+			return 0;
 		}
 	}
 }
